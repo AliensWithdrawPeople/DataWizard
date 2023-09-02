be5bed6a9ee5d3e2ed0fa020a7f2d72240e579e7
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2023-09-02 13:59:09 +0700

    Changed get_report interface, changed templates.

--- modules/aux_scripts/ReportForge/reporter.py
+++ modules/aux_scripts/ReportForge/reporter.py
@@ -16,12 +16,18 @@ from weasyprint import HTML
 class templates(Enum):
     VCM = 'VCM.html'
     UTM = 'UTM.html'
+    
+class img(Enum):
+    LOGO = 'Weatherford_logo.png'
+    STAMP = 'Weatherford_stamp.png'
+    SIGNATURE = 'ivanov_sign.png'
 
 root = os.path.dirname(os.path.abspath(__file__))
 
-def get_logo() -> str | None:
+def get_image(image_type: img | str) -> str | None:
     weatherford_logo = None
-    with open(pathlib.PurePath(root, 'html', 'Weatherford_Logo_small.png'), "rb") as image_file:
+    filename = image_type.value if type(image_type) is img else str(image_type)
+    with open(pathlib.PurePath(root, 'templates', filename), "rb") as image_file:
         weatherford_logo = 'data:image/png;base64,' + base64.b64encode(image_file.read()).decode()
     return weatherford_logo
 
@@ -29,13 +35,13 @@ def mk_if_not_exist(path: os.PathLike):
     if not os.path.isdir(path):
         os.mkdir(path)
 
-def get_report(id: int | str, config: list[tuple[templates, str]], zip_files: bool, **kwargs) -> pathlib.PurePath | list[pathlib.PurePath]:
+def get_report(id: int | str, config: list[tuple[templates, str, dict[str, str]]], zip_files: bool) -> pathlib.PurePath | list[pathlib.PurePath]:
     """_summary_
 
     id : int | str
         user id.
-    config : list[tuple[templates, str]]
-        list of (template_name, filename) tuples.
+    config : list[tuple[templates, str, dict[str, str]]]
+        list of (template_name, filename, parameters) tuples.
     zip_files : bool
         save files as zip
 
@@ -66,16 +72,16 @@ def get_report(id: int | str, config: list[tuple[templates, str]], zip_files: bo
     mk_if_not_exist(html_folder)
     mk_if_not_exist(pdf_folder)
     
-    weatherford_logo = get_logo()
-    outputs = []
-    for template_name, filename in config:
+    logo = get_image(img.LOGO)
+    stamp = get_image(img.STAMP)
+    outputs: list[pathlib.PurePath] = []
+    for template_name, filename, params in config:
         template = env.get_template(template_name.value)
         html_file_path = pathlib.PurePath(root, 'html', str(id), f'report_{template_name.value}')
         with open(html_file_path, 'w') as html_file:
-            html_file.write(template.render(weatherford_logo=weatherford_logo, **kwargs))
+            html_file.write(template.render(weatherford_logo=logo, stamp=stamp, **params))
     
         output_file_path = pathlib.PurePath(root, 'pdf', str(id), filename)
-        # TODO: Add margin.
         HTML(str(html_file_path)).write_pdf(str(output_file_path))
         outputs.append(output_file_path)
 
@@ -83,14 +89,14 @@ def get_report(id: int | str, config: list[tuple[templates, str]], zip_files: bo
         output_zip = pathlib.PurePath(root, 'pdf', f'{id}.zip')
         with ZipFile(output_zip, 'w') as myzip:
             for output in outputs:
-                myzip.write(output)
+                myzip.write(output, arcname=output.name)
         return output_zip
     
     return outputs
     
     
-template_params = {"title_add" : 'НК-061А0028 (22.02.25)',
-                    "report_number" : "03604",
+params_VCM = {"title_add" : 'НК-061А0028 (22.02.25)',
+                    "report_number" : "03604-VCM",
                     "date" : "30 августа 2023 г.",
                     "object_name" : 'Кран пробковый 3x3" 1502 MxF Master Valve',
                     "reg_number" : "WFT MV 12502", 
@@ -99,7 +105,6 @@ template_params = {"title_add" : 'НК-061А0028 (22.02.25)',
                     "batch_number" : "ИМЗ - 08.330.99", 
                     "number_1C" : "-", 
                     "location_and_kit_number" : 'БПО Ноябрьского филиала ООО "Везерфорд"  г.Ноябрьск Склад', 
-                    "standard" : "3997-00.001 МУ., ГОСТ 34004-2016., ГОСТ Р ЕН 13018-2014., РД 03-606-03., 0398.00.000 МУ.,  РД 03-606-03., СТО 9701105632-003-2021., ГОСТ Р ИСО 17637-2014.",
                     "tools" : ["Лупаизмерительная ЛИ-3-10 No230236", 'Линейка измерительная "Калиброн" No3158 от 18.04.23 г.', 'Штангенциркуль "Калиброн" 0-150 No102201991 от 18.04.23 г.', 
                             'Набор шаблонов  "Etalon" No 1 No2021491 от 18.04.23 г.', 'Набор шаблонов  "Etalon" No 3 No20201487 от 18.04.23 г.', 
                             'Шаблон универсальный сварщика УШС–3 No0783 от 18.04.23  г.', 'Магнитометр переносной ИМАГ-400Ц No2598 от 15.01.23 г.',],
@@ -109,11 +114,37 @@ template_params = {"title_add" : 'НК-061А0028 (22.02.25)',
                     "executor_position" : 'Специалист II уровня/Specialist of the 2nd level',
                     "identification" : '0041-2871',
                     "executor_name" : 'В.Р. Иванов',
-                    "stamp" : get_logo(),
-                    "facsimile" : get_logo()
+                    "facsimile" : get_image(img.SIGNATURE)
                 }
+params_UTM = {"title_add" : 'НК-061А0028 (22.02.25)',
+                    "report_number" : "03604-VCM",
+                    "date" : "30 августа 2023 г.",
+                    "object_name" : 'Кран пробковый 3x3" 1502 MxF Master Valve',
+                    "reg_number" : "WFT MV 12502", 
+                    "serial_number" : "0119", 
+                    "manufacturer" : "Омега", 
+                    "batch_number" : "ИМЗ - 08.330.99", 
+                    "number_1C" : "-", 
+                    "location_and_kit_number" : 'БПО Ноябрьского филиала ООО "Везерфорд"  г.Ноябрьск Склад', 
+                    "tools" : ["Толщиномер УТ-11 ЛУЧ  №0518 от 20.03.23 г.", "Преобразователь ПЭП П112-5-10/2-Т-003 №5404 от 19.03.23 г.", "Номинальная частота контроля: 5 МГц; СОП №7448."],
+                    "ambient_temp" : "+14",
+                    "general_light" : "541",
+                    "surf_light" : "1890",
+                    "executor_position" : 'Специалист II уровня/Specialist of the 2nd level',
+                    "identification" : '0041-2871',
+                    "executor_name" : 'В.Р. Иванов',
+                    "facsimile" : get_image(img.SIGNATURE),
+                    
+                    "ts" : [21.0] + 6 * [None],
+                    "minimal_ts" : [10.7] + 6 * [None],
+                    "sketch" : get_image('stock_sketch.png')
+                }
+
 import time
 start = time.perf_counter()
-get_report(1, [(templates.VCM, 'test.pdf')], zip_files = False, **template_params)
+# get_report(1, [(templates.VCM, 'test_VCM.pdf', params_VCM)], zip_files = False)
+
+
+get_report(1, [(templates.VCM, 'test_VCM.pdf', params_VCM), (templates.UTM, 'test_UTM.pdf', params_UTM),], zip_files = True)
 end = time.perf_counter()
 print(end - start)
